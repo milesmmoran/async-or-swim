103bbc0f97bc1c8bdaa07b53ec154e3cf2445cc7
Miles <miles@karmacheck.com>
2022-07-03 19:19:30 +0200

    added async wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-export const Greeter = (name: string) => `Hello ${name}`; 
+import { AxiosError, AxiosResponse } from 'axios';
+
+export interface AsyncResponse<ResponseType> {
+  readonly ok: boolean;
+  data?: ResponseType;
+  error?: AxiosError;
+}
+
+export class AsyncSuccessResponse<ResponseType> implements AsyncResponse<ResponseType> {
+  readonly ok: true = true;
+  data: ResponseType;
+
+  constructor(data: ResponseType) {
+    this.data = data;
+  }
+}
+
+export class AsyncErrorResponse<ResponseType> implements AsyncResponse<ResponseType> {
+  readonly ok: false = false;
+  error: AxiosError
+
+  constructor(error: any) {
+    this.error = error;
+  }
+}
+
+export const asyncOkErrorDataWrapper = async <ResponseType>(
+  asyncFunc: () => Promise<AxiosResponse<ResponseType>>
+) => {
+  try {
+    return Promise.resolve(new AsyncSuccessResponse<ResponseType>((await asyncFunc()).data));
+  } catch (error) {
+    return Promise.resolve(new AsyncErrorResponse<AxiosError>(error));
+  }
+};
